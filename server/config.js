d788e07e1a07dfbf98578fb8cf8da52d69cd9ad8
Anthony Zhang <azhang392@gmail.com>
2025-10-13 21:14:33 -0500

    Update Distro API key to correct working key

--- server/config.js
+++ server/config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 module.exports = {
   distro: {
-    apiKey: process.env.DISTRO_API_KEY || '32qCf5H}vfxl]|9(;$O*u33#JZiUl0ZY.',
+    apiKey: process.env.DISTRO_API_KEY || 'fB|#7qo1HyyQ#J$;+N_:BT|m*_x$}Z,&',
     apiEndpoint: process.env.DISTRO_API_ENDPOINT || 'https://pulse-chain-dc452eb2642a.herokuapp.com/api/external/news'
   }
 };
